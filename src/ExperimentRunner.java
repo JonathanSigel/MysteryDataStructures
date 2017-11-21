25badb5248bea0555acd66bcec5000e40eef10f3
Jonathan Sigel <jsigel@wpi.edu>
2017-11-21 12:17:14 -0500

    Used Kira's tests to build output methods, that average runtimes and prints them out. (Sample size: 30)

--- src/ExperimentRunner.java
+++ src/ExperimentRunner.java
@@ -8,12 +8,13 @@ public class ExperimentRunner {
 	private static final int NUM_DATA_STRUCTURES_TO_DEDUCE = 5;
 	private static final Random random = new Random(); //!!!is it fine that I just put this as a member variable so I don't have to constantly instantiate it?
 	private static final String CS_210X_TEAM_ID_FOR_PROJECT_4 = "ksegenchuk";
-
+	private static int[] N_VALUES = {1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90,
+			100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000,
+			6000, 7000, 8000, 9000, 10000};
 	public static void main (String[] args) {
-		System.out.println("---ADD---");
-		for (int i = 0; i < NUM_DATA_STRUCTURES_TO_DEDUCE; i++) {
-			System.out.println("Data Structure " + i + " : " + testAdd(i, 1000));
-		}
+		addTestOutput();
+		removeTestOutput();
+		containsTestOutput();
 	}
 
 	public static long testAdd(int dataStructureIndex, int n) {
@@ -51,7 +52,7 @@ public class ExperimentRunner {
 		return end - start;
 	}
 
-	public static Collection210X fill(int dataStructureIndex, int n) {
+	private static Collection210X fill(int dataStructureIndex, int n) {
 		Collection210X dataStructure = MysteryDataStructure.getMysteryDataStructure(CS_210X_TEAM_ID_FOR_PROJECT_4.hashCode(), dataStructureIndex, new Integer(0));
 
 		for (int i = 1; i < n; i++) { //starts at 1 because data structure was initialized with a value
@@ -59,5 +60,53 @@ public class ExperimentRunner {
 		}
 
 		return dataStructure;
+	}
+
+	private static void addTestOutput() {
+		System.out.println("---ADD---");
+
+		int counter = 0;
+		for (int i = 0; i < NUM_DATA_STRUCTURES_TO_DEDUCE; i++) {
+			System.out.println("Data Structure Being Tested: " + i);
+			for (int j = 0; j < N_VALUES.length; j++) {
+				for (int k = 0; k < 30; k++) {
+					counter += testAdd(i, N_VALUES[j]);
+				}
+				int average = counter / 30;
+				System.out.println(average);
+			}
+		}
+	}
+
+	public static void removeTestOutput() {
+		System.out.println("---REMOVE---");
+
+		int counter = 0;
+		for (int i = 0; i < NUM_DATA_STRUCTURES_TO_DEDUCE; i++) {
+			System.out.println("Data Structure Being Tested: " + i);
+			for (int j = 0; j < N_VALUES.length; j++) {
+				for (int k = 0; k < 30; k++) {
+					counter += testRemove(i, N_VALUES[j]);
+				}
+				int average = counter / 30;
+				System.out.println(average);
+			}
+		}
+	}
+
+	private static void containsTestOutput() {
+		System.out.println("---CONTAINS---");
+
+		int counter = 0;
+		for (int i = 0; i < NUM_DATA_STRUCTURES_TO_DEDUCE; i++) {
+			System.out.println("Data Structure Being Tested: " + i);
+			for (int j = 0; j < N_VALUES.length; j++) {
+				for (int k = 0; k < 30; k++) {
+					counter += testContains(i, N_VALUES[j]);
+				}
+				int average = counter / 30;
+				System.out.println(average);
+			}
+		}
 	}
 }
